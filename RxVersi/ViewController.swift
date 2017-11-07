75da9524b8379bdc100121dcf9a6a5fc56bba3e7
Marvin Böddeker <marvin.boeddeker@gsmg.de>
2017-11-07 21:53:20 +0100

    Init

---
+++ RxVersi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxVersi
+//
+//  Created by Marvin Böddeker on 07.11.17.
+//  Copyright © 2017 Marvin Böddeker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
